dccd0eec61bc5057ecd9a3fad6dd7118848572e6
james <grumpyjames@gmail.com>
2013-06-27 21:20:28 +0100

    tidying - move things around, delete some unused code

--- tic_tac_toe.hs
+++ tic_tac_toe.hs
@@ -2,6 +2,10 @@ import Data.List
 import Test.QuickCheck
 
 -- does the game have a result?
+resultN :: [[Mark]] -> Bool
+resultN rows = or[any result rows, any result (transpose rows)]
+
+-- does a row contain three in a row?
 result :: [Mark] -> Bool
 result = checkCount . foldl foldFn (0, Empty)
 
@@ -15,15 +19,22 @@ foldFn (count, a) b
 checkCount :: (Int, Mark) -> Bool
 checkCount (count, _) = count >= 3
 
-resultN :: [[Mark]] -> Bool
-resultN rows = or[any result rows, any result (transpose rows)]
-
 data Mark = Oh
           | Ex
           | Empty
           deriving (Show, Eq)
                    
 -- Properties, generators and some helpers
+
+prop_only_empty_elements_is_no_result xs = and [not $ elem Oh xs, not $ elem Ex xs] ==> result xs == False
+
+prop_three_in_a_row_has_a_result xs = or [[Oh, Oh, Oh] `isInfixOf` xs, [Ex, Ex, Ex] `isInfixOf` xs]  ==> result xs == True
+
+prop_three_in_any_row ttt = any (three_in_a_row) rows ==> resultN rows == True
+  where rows = unwrap ttt
+prop_all_rows_start_with_x ttt = all (headEquals Ex) rows ==> resultN rows == True 
+  where rows = unwrap ttt
+
 
 instance Arbitrary Mark where
   arbitrary = elements [Oh, Ex, Empty]
@@ -37,22 +48,7 @@ instance Arbitrary TicTacToe where
 unwrap :: TicTacToe -> [[Mark]]
 unwrap (TicTacToe rows) = rows 
 
-prop_only_empty_elements_is_no_result xs = and [not $ elem Oh xs, not $ elem Ex xs] ==> result xs == False
-
-prop_three_in_a_row_has_a_result xs = or [[Oh, Oh, Oh] `isInfixOf` xs, [Ex, Ex, Ex] `isInfixOf` xs]  ==> result xs == True
-
-prop_three_in_any_row ttt = any (three_in_a_row) rows ==> resultN rows == True
-  where rows = unwrap ttt
-prop_all_rows_start_with_x ttt = all (headEquals Ex) rows ==> resultN rows == True 
-  where rows = unwrap ttt
-
-n_by_n :: Int -> [[a]] -> Bool
-n_by_n desired_n rows = and[length rows == desired_n, all (lenEqual desired_n) rows]
-
 three_in_a_row xs = or [[Oh, Oh, Oh] `isInfixOf` xs, [Ex, Ex, Ex] `isInfixOf` xs]
-
-lenEqual :: Int -> [a] -> Bool
-lenEqual desired_len row = length row == desired_len
                               
 headEquals :: Mark -> [Mark] -> Bool                              
 headEquals x (y:ys) = x == y
