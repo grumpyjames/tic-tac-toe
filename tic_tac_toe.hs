58851be9cd55dd44e4ab5145b9ff9d28c0b1f668
james <grumpyjames@gmail.com>
2013-06-27 21:02:46 +0100

    generate a whole 3x3 tictactoe to help quickcheck not give up

--- tic_tac_toe.hs
+++ tic_tac_toe.hs
@@ -15,13 +15,8 @@ foldFn current@(count, mark) currentMark = if mark == currentMark
 checkCount :: (Int, Mark) -> Bool
 checkCount (count, mark) = and[count >= 3, not $ mark == Empty]
 
-resultN :: [GameRow] -> Bool
-resultN gameRows = or[any result rows, any result (transpose rows)]
-  where rows = unwrap gameRows
-
-unwrap :: [GameRow] -> [[Mark]]
-unwrap ((GameRow g):grs) = g:(unwrap grs) 
-unwrap [] = []
+resultN :: [[Mark]] -> Bool
+resultN rows = or[any result rows, any result (transpose rows)]
 
 data Mark = Oh
           | Ex
@@ -30,29 +25,33 @@ data Mark = Oh
 
 instance Arbitrary Mark where
   arbitrary = elements [Oh, Ex, Empty]
-  
-newtype GameRow = GameRow [Mark] deriving (Show, Eq)
-instance Arbitrary GameRow where
+
+newtype TicTacToe = TicTacToe [[Mark]] deriving (Show, Eq)
+instance Arbitrary TicTacToe where
   arbitrary = sized $ \s -> do
-    xs <- vectorOf 3 (elements [Oh, Ex, Empty])
-    return (GameRow xs)
+    rows <- vectorOf 3 (vectorOf 3 (elements [Oh, Ex, Empty]))
+    return (TicTacToe rows)
+
+unwrap :: TicTacToe -> [[Mark]]
+unwrap (TicTacToe rows) = rows 
 
 prop_only_empty_elements_is_no_result xs = and [not $ elem Oh xs, not $ elem Ex xs] ==> result xs == False
 
 prop_three_in_a_row_has_a_result xs = or [[Oh, Oh, Oh] `isInfixOf` xs, [Ex, Ex, Ex] `isInfixOf` xs]  ==> result xs == True
 
-prop_n_rows rows = and[any (three_in_a_row) rows, length rows == 3] ==> resultN rows == True
-
-prop_all_rows_start_with_x rows = and[all (headEquals Ex) rows, length rows == 3] ==> resultN rows == True 
+prop_three_in_any_row ttt = and[any (three_in_a_row) rows, length rows == 3] ==> (resultN rows) == True
+                  where rows = unwrap ttt
+prop_all_rows_start_with_x ttt = and[all (headEquals Ex) rows, length rows == 3] ==> (resultN rows) == True 
+                                 where rows = unwrap ttt
 
 n_by_n :: Int -> [[a]] -> Bool
 n_by_n desired_n rows = and[length rows == desired_n, all (lenEqual desired_n) rows]
 
-three_in_a_row (GameRow xs) = or [[Oh, Oh, Oh] `isInfixOf` xs, [Ex, Ex, Ex] `isInfixOf` xs]
+three_in_a_row xs = or [[Oh, Oh, Oh] `isInfixOf` xs, [Ex, Ex, Ex] `isInfixOf` xs]
 
 lenEqual :: Int -> [a] -> Bool
 lenEqual desired_len row = length row == desired_len
                               
-headEquals :: Mark -> GameRow -> Bool                              
-headEquals x (GameRow (y:ys)) = x == y
+headEquals :: Mark -> [Mark] -> Bool                              
+headEquals x (y:ys) = x == y
 headEquals _ _ = False
